b1175981cb2e2a11e2e1896c038a187d20ddc59e
Vitaliy <vitaliyanohin@gmail.com>
2019-05-30 12:29:13 +0300

    develop commit

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -12,7 +12,7 @@ public class ComplexNumber {
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (o == null || this.getClass() != o.getClass()) return false;
     ComplexNumber that = (ComplexNumber) o;
     return Double.compare(that.re, re) == 0 && Double.compare(that.im, im) == 0;
   }
